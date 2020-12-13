7d51f43b82f30844f2e24d9f1daca620efdb6cfd
rcazzato <74453387+rcazzato@users.noreply.github.com>
2020-12-13 12:36:34 +0100

    Add files via upload

---
+++ Dati.m
@@ -0,0 +1,17 @@
+% Dati
+% S0 è il prezzo del sottostante
+% E è prezzo di esercizio 
+% t0 è il tempo iniziale
+% T è la maturity
+% r è il tasso d'interesse
+% sig è la volatilità  
+% qdiv è il rateo di dividendo
+
+S0 = 123.62; 
+E = 130;
+sig = 0.5;
+r = 0.02;
+T = 0.04;
+t0 = 0;
+qdiv = 0;
+
